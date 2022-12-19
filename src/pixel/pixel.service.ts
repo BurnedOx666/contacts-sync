28b06b9566d3b590b8a17861f73ae4ef0369d28e
BurnedOx666 <mainak@oneplay.in>
2022-12-19 22:42:56 +0530

    data chunk syncing

--- src/pixel/pixel.service.ts
+++ src/pixel/pixel.service.ts
@@ -19,28 +19,34 @@ export class PixelService {
 
     Logger.log('Sync contacts : mongo data : ' + syncedContacts.length);
 
-    const bulkResponse = await this.searchService.indexContacts(
-      syncedContacts.map((c) => SerializersUtil.serializeContactDoc(c)),
-    );
+    const chunkSize = 50000;
 
-    if (bulkResponse.errors) {
-      const erroredDocuments = [];
-      // The items array has the same order of the dataset we just indexed.
-      // The presence of the `error` key indicates that the operation
-      // that we did for the document has failed.
-      bulkResponse.items.forEach((action, i) => {
-        const operation = Object.keys(action)[0];
-        if (action[operation].error) {
-          erroredDocuments.push({
-            // If the status is 429 it means that you can retry the document,
-            // otherwise it's very likely a mapping error, and you should
-            // fix the document before to try it again.
-            status: action[operation].status,
-            error: action[operation].error,
-          });
-        }
-      });
-      Logger.log('Sync errors', erroredDocuments);
+    for (let i = 0; i < syncedContacts.length; i += chunkSize) {
+      const chunk = syncedContacts.slice(i, i + chunkSize);
+      // do whatever
+      const bulkResponse = await this.searchService.indexContacts(
+        chunk.map((c) => SerializersUtil.serializeContactDoc(c)),
+      );
+
+      if (bulkResponse.errors) {
+        const erroredDocuments = [];
+        // The items array has the same order of the dataset we just indexed.
+        // The presence of the `error` key indicates that the operation
+        // that we did for the document has failed.
+        bulkResponse.items.forEach((action, i) => {
+          const operation = Object.keys(action)[0];
+          if (action[operation].error) {
+            erroredDocuments.push({
+              // If the status is 429 it means that you can retry the document,
+              // otherwise it's very likely a mapping error, and you should
+              // fix the document before to try it again.
+              status: action[operation].status,
+              error: action[operation].error,
+            });
+          }
+        });
+        Logger.log('Sync errors', erroredDocuments);
+      }
     }
 
     const count = await this.searchService.getContactsCount();
